841e20f54d2f3b24b5e3cd7e4a192f8fcf6ad5a5
djchie <derrickchie@Derricks-MacBook-Pro.local>
2018-02-19 02:52:24 -0800

    Updates the react native vector icons package

--- StarButton.js
+++ StarButton.js
@@ -13,24 +13,28 @@ import PropTypes from 'prop-types';
 import Button from 'react-native-button';
 import EntypoIcons from 'react-native-vector-icons/Entypo';
 import EvilIconsIcons from 'react-native-vector-icons/EvilIcons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
 import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
 import FoundationIcons from 'react-native-vector-icons/Foundation';
 import IoniconsIcons from 'react-native-vector-icons/Ionicons';
 import MaterialIconsIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIconsIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import OcticonsIcons from 'react-native-vector-icons/Octicons';
 import ZocialIcons from 'react-native-vector-icons/Zocial';
-import MaterialCommunityIconsIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SimpleLineIconsIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 const iconSets = {
   Entypo: EntypoIcons,
   EvilIcons: EvilIconsIcons,
+  Feather: FeatherIcons,
   FontAwesome: FontAwesomeIcons,
   Foundation: FoundationIcons,
   Ionicons: IoniconsIcons,
   MaterialIcons: MaterialIconsIcons,
+  MaterialCommunityIcons: MaterialCommunityIconsIcons,
   Octicons: OcticonsIcons,
   Zocial: ZocialIcons,
-  MaterialCommunityIcons: MaterialCommunityIconsIcons,
+  SimpleLineIcons: SimpleLineIconsIcons,
 };
 
 const propTypes = {
