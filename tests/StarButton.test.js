65b1cb500f6d3e5c611f796816d9a538fcb4b55e
jayzamazing <adrianjaylopez@gmail.com>
2018-04-12 16:03:03 -0400

    (fix/test) Addition of repo's needed for testing and updated StarButton.test.js to run without warnings.

--- tests/StarButton.test.js
+++ tests/StarButton.test.js
@@ -6,6 +6,22 @@ import StarButton from '../StarButton';
 // https://medium.com/react-native-training/learning-to-test-react-native-with-jest-part-1-f782c4e30101
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<StarButton />).toJSON();
+  const rendered = renderer.create(
+  <StarButton 
+  disabled={true}
+  maxStars={5}
+  rating={4}
+  starSize={10}
+  starColor={'#FFFFFF'}
+  emptyStarColor={'#000000'}
+  emptyStar={'ios-star'}
+  fullStar={'ios-star'}
+  halfStar={'ios-star-half'}
+  halfStarEnabled={true}
+  iconSet={'Ionicons'}
+  reversed={false}
+  starIconName={'ios-add-circle'}
+  onStarButtonPress={(rating) => this.onStarRatingPress(rating)}
+  buttonStyle={{}}/>).toJSON();
   expect(rendered).toBeTruthy();
 });
